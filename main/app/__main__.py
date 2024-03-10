944f725d9848d719467ef082d6a5bbf61078d6ab
PLAYPLAK <131369587+PLAYPLAK@users.noreply.github.com>
2024-03-10 10:21:49 +0700

    update remove __mian__.py

--- main/app/__main__.py
+++
@@ -1,6 +0,0 @@
-from app import app, bottest
-from config1 import CLIENT_SECRET, TOKEN, OAUTH_URL, REDIRECT_URL
-
-if __name__ == "__main__":
-    bottest.run(TOKEN)
-    app.run(debug=True, port=5000)
